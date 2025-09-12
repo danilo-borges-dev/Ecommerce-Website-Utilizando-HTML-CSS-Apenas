16a1eb6303c7604fa3bfb2e5f2763e037d1af54b
Danilo Borges <danilosilva2017@hotmail.com>
2025-09-12 07:02:34 -0300

    feat(menu-top-responsive): adicionar responsividade no menu de topo.
    - Ajustado o  espaço (padding) à direita da logo e à esquerda do menu hamburger
    - Abaixo de 750px o menu deve sumir e dar espaço ao ícone do hamburger com os respectivos menus
    - O texto principal também vai diminuir
    - Ao clicar sobre o ícone do hamburger será alterado para um x
    - O botão de comprar também vai ser redimensionado

--- app.js
+++ app.js
@@ -1,5 +1,47 @@
+/**
+ * Seleciona o elemento <header> do documento.
+ * O header receberá a classe "sticky" sempre que o usuário rolar a página
+ * para baixo (scrollY > 0).
+ */
 const header = document.querySelector("header");
 
+/**
+ * Adiciona um listener para o evento de scroll da janela.
+ *
+ * @event scroll
+ * @param {Event} event - O evento de scroll disparado pela janela.
+ *
+ * @description
+ * Alterna a classe "sticky" no elemento <header> quando a rolagem vertical
+ * da janela for maior que zero. Isso permite aplicar estilos fixos (ex:
+ * header fixo no topo).
+ */
 window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", this.window.scrollY > 0);
 });
+
+/**
+ * Seleciona o ícone do menu (hambúrguer).
+ * @type {HTMLElement}
+ */
+let menu = document.querySelector("#menu-icon");
+
+/**
+ * Seleciona o menu de navegação.
+ * @type {HTMLElement}
+ */
+let navMenu = document.querySelector(".nav-menu");
+
+/**
+ * Função de clique no ícone do menu.
+ *
+ * @event click
+ * @description
+ * Alterna as classes de estilo do menu:
+ * - "bx-x": muda o ícone (ex: de hambúrguer para X).
+ * - "open": abre ou fecha o menu de navegação.
+ */
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navMenu.classList.toggle("open");
+};
